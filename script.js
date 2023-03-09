3aa6b00dff879dc327746b9ec8e6d6e101e7b3b9
ARNAUD CHANE <arnaud.chane.dev@gmail.com>
2023-03-09 10:41:59 +0100

    I explained all the steps and at the end, the best is to unstaged files before modified

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ let UX = "Second Try UserEx";
 
 // Second comment 10h28
 
+// after the git reset --soft
+
+// First add script file staging, change it add it again to staging, commit and push
+// only the first version was sent >> So need to unstaged the file before modified it
+
+
+
+
 
